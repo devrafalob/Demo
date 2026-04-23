bef0886bfabaeb43abc46fa7762505367d340830
Rafael <rafael.lobao104@academico.ifs.edu.br>
2026-04-23 19:59:15 -0300

    Student class

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -13,4 +13,79 @@ public class Student {
         this.id = id;
     }
 
+    public Student() {
+    }
+
+    public Student(String name,
+                   Long id,
+                   String email,
+                   LocalDate dov,
+                   Integer age) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.dov = dov;
+        this.age = age;
+    }
+
+    public Student(String name,
+                   String email,
+                   LocalDate dov,
+                   Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dov = dov;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDov() {
+        return dov;
+    }
+
+    public void setDov(LocalDate dov) {
+        this.dov = dov;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dov=" + dov +
+                ", age=" + age +
+                '}';
+    }
 }

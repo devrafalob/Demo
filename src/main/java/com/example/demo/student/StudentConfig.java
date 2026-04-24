0c94e671a21c157218700e8a559ab3ee5f99eaf6
Rafael <rafael.lobao104@academico.ifs.edu.br>
2026-04-24 17:50:37 -0300

    Test:Create new students

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner
+            (StudentRepository repository){
+        return args -> {
+            Student Maria = new Student(
+                    "Maria",
+                    "",
+                    LocalDate.of(2009, Month.AUGUST, 5),
+                    18
+            );
+            Student Pedro = new Student(
+                    "Pedro",
+                    "",
+                    LocalDate.of(2004, Month.AUGUST, 5),
+                    21
+            );
+            repository.saveAll(
+                    List.of(Maria,  Pedro)
+            );
+        };
+    }
+}
